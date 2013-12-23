bcd5454c2cceea513a212cb4a120be6ed93fa186
git-goblin <gitgoblin@gmail.com>
2013-12-23 10:00:24 +0000

    Added features to class to resolve #4
    Added a method and a public datatable to fill the ComboBox with values.
    Also deleted adminComboBoxAmmed to start fresh and try and use this new
    approach to resolve the ComboBox issue.

--- srdb/DBConnect.cs
+++ srdb/DBConnect.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
+
+//using System.ComponentModel;
+using System.Data;
+
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient; //required for MySQL Connections
 using System.Security.Cryptography; //required for generating SHA1 values
@@ -152,6 +156,52 @@ namespace srdb
                 return false;
             }
         }
-      
+        
+           public void combobox_initialise()
+        {
+            server = "localhost";
+            database = "combobox";
+            uid = "root";
+            password = "MySQuireL321!";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            login_connection = new MySqlConnection(connectionString);
+        }
+        
+        
+        //open connection to database
+        public bool combobox_Open_Connection()
+        {
+            try
+            {
+                connection.Close();            
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+        public DataTable table = new DataTable();
+        public void fill_data(string db_table)
+        {
+            MySqlDataAdapter da = new MySqlDataAdapter(db_table, connection);
+            da.Fill(table);
+        }
+
     }
 }
