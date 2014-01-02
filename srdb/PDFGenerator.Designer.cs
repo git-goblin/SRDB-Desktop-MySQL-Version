d2d114398e7dcafe486d3ba2cec15259a5c0e151
git-goblin <gitgoblin@gmail.com>
2014-01-02 15:16:22 +0000

    PDF Creator Form
    Using a PDF Creation API, a new form has been created to generate PDF
    invoices

---
+++ srdb/PDFGenerator.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace srdb
+{
+    partial class PDFGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDFGenerator));
+            this.btnCreatePDF = new System.Windows.Forms.Button();
+            this.txtSRID = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreatePDF
+            // 
+            this.btnCreatePDF.Location = new System.Drawing.Point(242, 10);
+            this.btnCreatePDF.Name = "btnCreatePDF";
+            this.btnCreatePDF.Size = new System.Drawing.Size(93, 23);
+            this.btnCreatePDF.TabIndex = 0;
+            this.btnCreatePDF.Text = "Create PDF";
+            this.btnCreatePDF.UseVisualStyleBackColor = true;
+            this.btnCreatePDF.Click += new System.EventHandler(this.btnCreatePDF_Click);
+            // 
+            // txtSRID
+            // 
+            this.txtSRID.Location = new System.Drawing.Point(136, 12);
+            this.txtSRID.Name = "txtSRID";
+            this.txtSRID.Size = new System.Drawing.Size(100, 20);
+            this.txtSRID.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(32, 15);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(98, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Service Record ID:";
+            // 
+            // PDFGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 125);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtSRID);
+            this.Controls.Add(this.btnCreatePDF);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PDFGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Generator";
+            this.Load += new System.EventHandler(this.PDFGenerator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreatePDF;
+        private System.Windows.Forms.TextBox txtSRID;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
---
+++ srdb/PDFGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//using iTextSharp.text.pdf;
+//using iTextSharp.text;  
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Diagnostics;
+
+namespace srdb
+{
+    public partial class PDFGenerator : Form
+    {
+        public PDFGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void PDFGenerator_Load(object sender, EventArgs e)
+        {
+            //form on load
+        }
+
+        private void btnCreatePDF_Click(object sender, EventArgs e)
+        {
+          
+           // SaveFileDialog sfd = new SaveFileDialog();
+         //   sfd.ShowDialog();
+            var document = new Document();
+            string path = @"C:\PDF";
+            PdfWriter.GetInstance(document, new FileStream(path + "/Tables.pdf", FileMode.Create));
+
+            document.Open();
+            document.Add(new Paragraph("My First PDF"));
+            PdfPTable table = new PdfPTable(3);
+            PdfPCell cell = new PdfPCell(new Phrase("Curriemotors Invoice"));
+            cell.Colspan = 3;
+            cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+            table.AddCell(cell);
+            table.AddCell("Value 1");
+            table.AddCell("Value 2");
+            table.AddCell("Value 3");
+            document.Add(table);
+            document.Close();
+        }
+    }
+}
