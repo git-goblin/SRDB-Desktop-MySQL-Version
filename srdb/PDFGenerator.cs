02aa34a37d141d3aec42947a8e1f510451b3c421
git-goblin <gitgoblin@gmail.com>
2014-01-02 16:01:25 +0000

    Create PDF and Pull values from DB
    Two functions now created: one to pull from the DB and assign values to
    variables and another to generate the PDF.

--- srdb/PDFGenerator.cs
+++ srdb/PDFGenerator.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data.MySqlClient;
 //using iTextSharp.text.pdf;
 //using iTextSharp.text;  
 
@@ -20,37 +21,103 @@ namespace srdb
 {
     public partial class PDFGenerator : Form
     {
+        private DBConnect dbConnect;
+        private validate val;
         public PDFGenerator()
         {
+            val = new validate();
+            dbConnect = new DBConnect();
             InitializeComponent();
         }
 
+        String Firstname, Surname, Address1, Address2, Postcode, Registration, carModel, dateSold, Invoicenumber, soldBy, salesBranch, carType, paymentMethod, Total, Invoicetotal, numberofServices, CommissionAmount;
         private void PDFGenerator_Load(object sender, EventArgs e)
         {
             //form on load
         }
 
+
+        private void pull_from_db()
+        {
+            try
+            {
+                dbConnect.Initialize();
+                dbConnect.OpenConnection();
+                string ID = txtSRID.Text;
+                if (val.validate_id(ID) != 1)
+                {
+                    return;
+                }
+
+                string selectID = "SELECT * FROM records WHERE ID=@ID";
+                using (MySqlCommand cmd = new MySqlCommand(selectID, dbConnect.connection))
+                {
+                    cmd.Parameters.AddWithValue("@ID", ID);
+                    using (MySqlDataReader read = cmd.ExecuteReader())
+                    {
+                        while (read.Read())
+                        //Gets the value by column name
+                        {
+                            Firstname = read.GetString(read.GetOrdinal("firstName"));
+                            Surname = read.GetString(read.GetOrdinal("surName"));
+                            Address1 = read.GetString(read.GetOrdinal("address1"));
+                            Address2 = read.GetString(read.GetOrdinal("address2"));
+                            Postcode = read.GetString(read.GetOrdinal("postcode"));
+                            Registration = read.GetString(read.GetOrdinal("registration"));
+                            carModel = read.GetString(read.GetOrdinal("model"));
+                            dateSold = read.GetString(read.GetOrdinal("date_sold"));
+                            Invoicenumber = read.GetString(read.GetOrdinal("invoice_number"));
+                            soldBy = read.GetString(read.GetOrdinal("sold_by"));
+                            salesBranch = read.GetString(read.GetOrdinal("sales_branch"));
+                            carType = read.GetString(read.GetOrdinal("type"));
+                            paymentMethod = read.GetString(read.GetOrdinal("payment_method"));
+                            Total = read.GetString(read.GetOrdinal("total"));
+                            Invoicetotal = read.GetString(read.GetOrdinal("invoice_total"));
+                            numberofServices = read.GetString(read.GetOrdinal("number_of_services"));
+                            CommissionAmount = read.GetString(read.GetOrdinal("commission_amount"));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error with pulling values from the DB " + ex);
+            }
+        }
+
+        private void create_PDF()
+        {
+            try
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog(); //Asks the user to choose a file where the PDF will be saved
+                fbd.ShowDialog();
+                string u_path = fbd.SelectedPath;
+                var document = new Document();
+                PdfWriter.GetInstance(document, new FileStream(u_path + "/Tables.pdf", FileMode.Create));
+                document.Open();
+                document.Add(new Paragraph("My First PDF"));
+                PdfPTable table = new PdfPTable(3);
+                PdfPCell cell = new PdfPCell(new Phrase("Curriemotors Invoice"));
+                cell.Colspan = 3;
+                cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+                table.AddCell(cell);
+                table.AddCell("Value 1");
+                table.AddCell("Value 2");
+                table.AddCell("Value 3");
+                document.Add(table);
+                document.Close();
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating PDF! " + ex);
+            }
+            MessageBox.Show("PDF Successfully created!");
+        }
+
         private void btnCreatePDF_Click(object sender, EventArgs e)
         {
-          
-           // SaveFileDialog sfd = new SaveFileDialog();
-         //   sfd.ShowDialog();
-            var document = new Document();
-            string path = @"C:\PDF";
-            PdfWriter.GetInstance(document, new FileStream(path + "/Tables.pdf", FileMode.Create));
-
-            document.Open();
-            document.Add(new Paragraph("My First PDF"));
-            PdfPTable table = new PdfPTable(3);
-            PdfPCell cell = new PdfPCell(new Phrase("Curriemotors Invoice"));
-            cell.Colspan = 3;
-            cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
-            table.AddCell(cell);
-            table.AddCell("Value 1");
-            table.AddCell("Value 2");
-            table.AddCell("Value 3");
-            document.Add(table);
-            document.Close();
+            pull_from_db();
+            create_PDF();
         }
     }
 }
