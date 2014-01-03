276849e42053e263fb290120192a98ef94db5d26
git-goblin <gitgoblin@gmail.com>
2014-01-03 16:33:09 +0000

    Create Table for Invoice
    More invoice-like table system created in a new function called
    "invoice_PDF", using an array to generate the table size (
    inv_table.SetWidths(new int[] { 1, 4 }); ) when the guideline for the
    invoice is set, this number will be fixed.

--- srdb/PDFGenerator.cs
+++ srdb/PDFGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+//using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -113,6 +113,34 @@ namespace srdb
                 MessageBox.Show("Error creating PDF! " + ex, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             
+        }
+
+        private void invoice_PDF()
+        {
+            String ID = txtSRID.Text;
+            string file_name = "Invoice - " + Invoicenumber + " From Service Record ID - " + ID;
+            FolderBrowserDialog fbd = new FolderBrowserDialog(); //Asks the user to choose a file where the PDF will be saved
+            fbd.ShowDialog();
+            Font arial = FontFactory.GetFont("Arial", 12, BaseColor.GRAY);
+            string u_path = fbd.SelectedPath;
+            var document = new Document();
+            PdfWriter.GetInstance(document, new FileStream(u_path + "/" + file_name + ".pdf", FileMode.Create));
+            document.Open();
+            PdfPTable inv_table = new PdfPTable(2);
+            inv_table.HorizontalAlignment = 0;
+            inv_table.SpacingBefore = 10;
+            inv_table.SpacingAfter = 10;
+            inv_table.DefaultCell.Border = 0;
+            inv_table.SetWidths(new int[] { 1, 4 });
+
+            inv_table.AddCell(new Phrase("Invoice Number: ", arial));
+            inv_table.AddCell(new Phrase(Invoicenumber, arial));
+            inv_table.AddCell(new Phrase("Name: ", arial));
+            inv_table.AddCell(new Phrase(Firstname + " " + Surname, arial));
+
+            document.Add(inv_table);
+
+            document.Close();
         }
 
         private void btnCreatePDF_Click(object sender, EventArgs e)
