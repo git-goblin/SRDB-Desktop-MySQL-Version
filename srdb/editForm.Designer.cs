cd757cc5fb21e333f95eae524ae8405d6828a2b2
git-goblin <gitgoblin@gmail.com>
2014-01-14 16:52:15 +0000

    Text Field Added
    SRID field added

--- srdb/editForm.Designer.cs
+++ srdb/editForm.Designer.cs
@@ -69,13 +69,15 @@
             this.txtCommissionAmount = new System.Windows.Forms.TextBox();
             this.label17 = new System.Windows.Forms.Label();
             this.checkBoxVAT = new System.Windows.Forms.CheckBox();
+            this.txtSRID = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // soldBy
             // 
             this.soldBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.soldBy.FormattingEnabled = true;
-            this.soldBy.Location = new System.Drawing.Point(79, 280);
+            this.soldBy.Location = new System.Drawing.Point(79, 283);
             this.soldBy.Name = "soldBy";
             this.soldBy.Size = new System.Drawing.Size(143, 21);
             this.soldBy.TabIndex = 70;
@@ -92,7 +94,7 @@
             // 
             // btnSaveChanges
             // 
-            this.btnSaveChanges.Location = new System.Drawing.Point(311, 273);
+            this.btnSaveChanges.Location = new System.Drawing.Point(311, 287);
             this.btnSaveChanges.Name = "btnSaveChanges";
             this.btnSaveChanges.Size = new System.Drawing.Size(133, 34);
             this.btnSaveChanges.TabIndex = 68;
@@ -102,7 +104,7 @@
             // 
             // btnClearform
             // 
-            this.btnClearform.Location = new System.Drawing.Point(311, 211);
+            this.btnClearform.Location = new System.Drawing.Point(311, 242);
             this.btnClearform.Name = "btnClearform";
             this.btnClearform.Size = new System.Drawing.Size(133, 34);
             this.btnClearform.TabIndex = 67;
@@ -206,7 +208,7 @@
             // 
             this.carType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.carType.FormattingEnabled = true;
-            this.carType.Location = new System.Drawing.Point(79, 334);
+            this.carType.Location = new System.Drawing.Point(79, 337);
             this.carType.Name = "carType";
             this.carType.Size = new System.Drawing.Size(143, 21);
             this.carType.TabIndex = 58;
@@ -214,7 +216,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(39, 337);
+            this.label12.Location = new System.Drawing.Point(39, 340);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(34, 13);
             this.label12.TabIndex = 57;
@@ -224,7 +226,7 @@
             // 
             this.salesBranch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.salesBranch.FormattingEnabled = true;
-            this.salesBranch.Location = new System.Drawing.Point(79, 307);
+            this.salesBranch.Location = new System.Drawing.Point(79, 310);
             this.salesBranch.Name = "salesBranch";
             this.salesBranch.Size = new System.Drawing.Size(143, 21);
             this.salesBranch.TabIndex = 56;
@@ -232,7 +234,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(10, 312);
+            this.label11.Location = new System.Drawing.Point(10, 315);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(73, 13);
             this.label11.TabIndex = 55;
@@ -241,7 +243,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(36, 284);
+            this.label10.Location = new System.Drawing.Point(36, 287);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(45, 13);
             this.label10.TabIndex = 54;
@@ -249,7 +251,7 @@
             // 
             // txtInvoicenumber
             // 
-            this.txtInvoicenumber.Location = new System.Drawing.Point(104, 255);
+            this.txtInvoicenumber.Location = new System.Drawing.Point(104, 268);
             this.txtInvoicenumber.Name = "txtInvoicenumber";
             this.txtInvoicenumber.Size = new System.Drawing.Size(118, 20);
             this.txtInvoicenumber.TabIndex = 53;
@@ -257,7 +259,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(13, 258);
+            this.label9.Location = new System.Drawing.Point(13, 261);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(85, 13);
             this.label9.TabIndex = 52;
@@ -265,7 +267,7 @@
             // 
             // dateSold
             // 
-            this.dateSold.Location = new System.Drawing.Point(79, 229);
+            this.dateSold.Location = new System.Drawing.Point(79, 232);
             this.dateSold.Name = "dateSold";
             this.dateSold.Size = new System.Drawing.Size(143, 20);
             this.dateSold.TabIndex = 51;
@@ -273,7 +275,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(23, 232);
+            this.label8.Location = new System.Drawing.Point(23, 235);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(57, 13);
             this.label8.TabIndex = 50;
@@ -284,7 +286,7 @@
             this.carModel.DisplayMember = "(none)";
             this.carModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.carModel.FormattingEnabled = true;
-            this.carModel.Location = new System.Drawing.Point(79, 202);
+            this.carModel.Location = new System.Drawing.Point(79, 205);
             this.carModel.Name = "carModel";
             this.carModel.Size = new System.Drawing.Size(143, 21);
             this.carModel.TabIndex = 49;
@@ -292,7 +294,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(41, 207);
+            this.label7.Location = new System.Drawing.Point(41, 210);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(39, 13);
             this.label7.TabIndex = 48;
@@ -300,7 +302,7 @@
             // 
             // txtRegistration
             // 
-            this.txtRegistration.Location = new System.Drawing.Point(79, 176);
+            this.txtRegistration.Location = new System.Drawing.Point(79, 179);
             this.txtRegistration.Name = "txtRegistration";
             this.txtRegistration.Size = new System.Drawing.Size(143, 20);
             this.txtRegistration.TabIndex = 47;
@@ -308,7 +310,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 179);
+            this.label6.Location = new System.Drawing.Point(16, 182);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(66, 13);
             this.label6.TabIndex = 46;
@@ -316,7 +318,7 @@
             // 
             // txtPostcode
             // 
-            this.txtPostcode.Location = new System.Drawing.Point(79, 150);
+            this.txtPostcode.Location = new System.Drawing.Point(79, 153);
             this.txtPostcode.Name = "txtPostcode";
             this.txtPostcode.Size = new System.Drawing.Size(143, 20);
             this.txtPostcode.TabIndex = 45;
@@ -324,7 +326,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(23, 153);
+            this.label5.Location = new System.Drawing.Point(23, 156);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(55, 13);
             this.label5.TabIndex = 44;
@@ -332,7 +334,7 @@
             // 
             // txtAddress2
             // 
-            this.txtAddress2.Location = new System.Drawing.Point(79, 124);
+            this.txtAddress2.Location = new System.Drawing.Point(79, 127);
             this.txtAddress2.Name = "txtAddress2";
             this.txtAddress2.Size = new System.Drawing.Size(143, 20);
             this.txtAddress2.TabIndex = 43;
@@ -340,7 +342,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 127);
+            this.label4.Location = new System.Drawing.Point(23, 130);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(57, 13);
             this.label4.TabIndex = 42;
@@ -348,7 +350,7 @@
             // 
             // txtAddress1
             // 
-            this.txtAddress1.Location = new System.Drawing.Point(79, 98);
+            this.txtAddress1.Location = new System.Drawing.Point(79, 101);
             this.txtAddress1.Name = "txtAddress1";
             this.txtAddress1.Size = new System.Drawing.Size(143, 20);
             this.txtAddress1.TabIndex = 41;
@@ -356,7 +358,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 101);
+            this.label3.Location = new System.Drawing.Point(23, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 13);
             this.label3.TabIndex = 40;
@@ -364,7 +366,7 @@
             // 
             // txtSurname
             // 
-            this.txtSurname.Location = new System.Drawing.Point(79, 72);
+            this.txtSurname.Location = new System.Drawing.Point(79, 75);
             this.txtSurname.Name = "txtSurname";
             this.txtSurname.Size = new System.Drawing.Size(143, 20);
             this.txtSurname.TabIndex = 39;
@@ -372,7 +374,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 75);
+            this.label2.Location = new System.Drawing.Point(23, 78);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 38;
@@ -380,7 +382,7 @@
             // 
             // txtFirstname
             // 
-            this.txtFirstname.Location = new System.Drawing.Point(79, 46);
+            this.txtFirstname.Location = new System.Drawing.Point(79, 49);
             this.txtFirstname.Name = "txtFirstname";
             this.txtFirstname.Size = new System.Drawing.Size(143, 20);
             this.txtFirstname.TabIndex = 37;
@@ -388,7 +390,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 49);
+            this.label1.Location = new System.Drawing.Point(23, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 36;
@@ -396,11 +398,11 @@
             // 
             // btnSelectID
             // 
-            this.btnSelectID.Location = new System.Drawing.Point(44, 12);
+            this.btnSelectID.Location = new System.Drawing.Point(156, 12);
             this.btnSelectID.Name = "btnSelectID";
-            this.btnSelectID.Size = new System.Drawing.Size(178, 23);
+            this.btnSelectID.Size = new System.Drawing.Size(66, 23);
             this.btnSelectID.TabIndex = 71;
-            this.btnSelectID.Text = "Select Service Record ID";
+            this.btnSelectID.Text = "Select ID";
             this.btnSelectID.UseVisualStyleBackColor = true;
             this.btnSelectID.Click += new System.EventHandler(this.btnSelectID_Click);
             // 
@@ -438,11 +440,29 @@
             this.checkBoxVAT.Text = "Auto Calculate VAT";
             this.checkBoxVAT.UseVisualStyleBackColor = true;
             // 
+            // txtSRID
+            // 
+            this.txtSRID.Location = new System.Drawing.Point(349, 197);
+            this.txtSRID.Name = "txtSRID";
+            this.txtSRID.Size = new System.Drawing.Size(95, 20);
+            this.txtSRID.TabIndex = 78;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(245, 200);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(98, 13);
+            this.label18.TabIndex = 77;
+            this.label18.Text = "Service Record ID:";
+            // 
             // editForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 382);
+            this.ClientSize = new System.Drawing.Size(471, 390);
+            this.Controls.Add(this.txtSRID);
+            this.Controls.Add(this.label18);
             this.Controls.Add(this.checkBoxVAT);
             this.Controls.Add(this.txtCommissionAmount);
             this.Controls.Add(this.label17);
@@ -535,5 +555,7 @@
         private System.Windows.Forms.TextBox txtCommissionAmount;
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.CheckBox checkBoxVAT;
+        private System.Windows.Forms.TextBox txtSRID;
+        private System.Windows.Forms.Label label18;
     }
 }
