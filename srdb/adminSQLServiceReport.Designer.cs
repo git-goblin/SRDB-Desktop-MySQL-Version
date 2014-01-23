d0e3d6764c8603dc01b45c290a7150ce1bdfe004
git-goblin <gitgoblin@gmail.com>
2014-01-23 10:33:15 +0000

    New SQL search form
    New form adminSQLServiceReport created so that reports can be created
    using SQL queries and passed into an Excel spreadsheet

---
+++ srdb/adminSQLServiceReport.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace srdb
+{
+    partial class adminSQLServiceReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(adminSQLServiceReport));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.linkLabelSQL = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(447, 3);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.Location = new System.Drawing.Point(447, 32);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(75, 23);
+            this.btnMainMenu.TabIndex = 1;
+            this.btnMainMenu.Text = "Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            this.btnMainMenu.Click += new System.EventHandler(this.btnMainMenu_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SQL Query:";
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(66, 6);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(375, 20);
+            this.txtQuery.TabIndex = 3;
+            // 
+            // linkLabelSQL
+            // 
+            this.linkLabelSQL.AutoSize = true;
+            this.linkLabelSQL.Location = new System.Drawing.Point(102, 32);
+            this.linkLabelSQL.Name = "linkLabelSQL";
+            this.linkLabelSQL.Size = new System.Drawing.Size(298, 26);
+            this.linkLabelSQL.TabIndex = 4;
+            this.linkLabelSQL.TabStop = true;
+            this.linkLabelSQL.Text = "If you are unsure what SQL syntax to use, make sure you \r\nfollow this link or con" +
+    "tact the DBA before running SQL queries";
+            this.linkLabelSQL.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.linkLabelSQL.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSQL_LinkClicked);
+            // 
+            // adminSQLServiceReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 63);
+            this.Controls.Add(this.linkLabelSQL);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMainMenu);
+            this.Controls.Add(this.btnGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "adminSQLServiceReport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin SQL Service Report";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.LinkLabel linkLabelSQL;
+    }
+}
---
+++ srdb/adminSQLServiceReport.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+//add this to create Excel documents -->
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using System.IO;
+using System.Diagnostics;
+
+namespace srdb
+{
+    public partial class adminSQLServiceReport : Form
+    {
+        private DBConnect dbConnect;
+        private DataTable sr_table = new DataTable();
+        public adminSQLServiceReport()
+        {
+            dbConnect = new DBConnect();
+            InitializeComponent();
+        }
+
+        private void fill_table()
+        {
+            String report_query = txtQuery.Text;
+            dbConnect.services_initialise();
+            dbConnect.services_Open_Connection();
+            using (MySqlDataAdapter da = new MySqlDataAdapter(report_query, dbConnect.services_connection)) //create a new DataAdaptor
+            {
+                da.Fill(sr_table); //File the table with the values from the DataAdaptor
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String sql_query = txtQuery.Text;
+                if (sql_query == "" || sql_query.Length < 10)
+                {
+                    MessageBox.Show("Invalid SQL query! ", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                FolderBrowserDialog fbd = new FolderBrowserDialog(); //Asks the user to choose a file where the PDF will be saved
+                fbd.ShowDialog();
+                string u_path = fbd.SelectedPath;
+                String date = Convert.ToString(DateTime.Now.Date);
+                string file_name = "Report Created - " + date;
+                using (ExcelPackage excelpkg = new ExcelPackage())
+                {
+                    excelpkg.Workbook.Properties.Title = "End of Month Report"; //Adds a title
+                    excelpkg.Workbook.Worksheets.Add("Report"); //creates a new worksheet
+                    ExcelWorksheet ws = excelpkg.Workbook.Worksheets[1]; //postion of the worksheet
+                    ws.Name = "EoM Report";
+
+                    var cell_SRID = ws.Cells[1, 1]; //declare the cells
+                    var cell_date = ws.Cells[1, 2];
+                    var cell_firstName = ws.Cells[1, 3];
+                    var cell_surName = ws.Cells[1, 4];
+                    var cell_amount = ws.Cells[1, 5];
+                    var cell_services_remaining = ws.Cells[1, 6];
+                    var cell_services_left = ws.Cells[1, 7];
+                    var cell_invoice_number = ws.Cells[1, 8];
+
+                    cell_SRID.Value = "SRID"; //sets a value to the cells
+                    cell_date.Value = "Date";
+                    cell_firstName.Value = "Firstname";
+                    cell_surName.Value = "Surname";
+                    cell_amount.Value = "Amount";
+                    cell_services_remaining.Value = "Services Remaining";
+                    cell_services_left.Value = "Services Left";
+                    cell_invoice_number.Value = "Invoice Number";
+
+                    fill_table();
+
+                    ws.Cells["A3"].LoadFromDataTable(sr_table, false);
+
+                    Byte[] bin = excelpkg.GetAsByteArray();
+                    File.WriteAllBytes(u_path + "/" + file_name + ".xlsx", bin); //writes to the file, needs to be XLSX format
+                }
+                MessageBox.Show("Report created!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error generating report! " + ex, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            adminControlMenu acm = new adminControlMenu();
+            acm.Show();
+        }
+
+        private void linkLabelSQL_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("http://www.w3schools.com/sql/");
+            Process.Start(sInfo);
+        }
+    }
+}
