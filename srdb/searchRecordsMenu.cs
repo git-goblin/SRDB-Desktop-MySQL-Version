b3af5cc3d24bbe24acf223eba3f459df2318cd21
git-goblin <gitgoblin@gmail.com>
2014-01-31 15:44:41 +0000

    Main Menu button added
    searchRecordsMenu now has a mainMenu button.

--- srdb/searchRecordsMenu.cs
+++ srdb/searchRecordsMenu.cs
@@ -51,5 +51,12 @@ namespace srdb
             searchServiceRecordID ssrid = new searchServiceRecordID();
             ssrid.Show();
         }
+
+        private void btnMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            mainMenu mm = new mainMenu();
+            mm.Show();
+        }
     }
 }
