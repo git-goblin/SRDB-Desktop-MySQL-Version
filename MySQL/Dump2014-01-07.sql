61976c7f8cdb90c2a4741e8b2fb024b9c390b88b
git-goblin <gitgoblin@gmail.com>
2014-02-04 09:06:00 +0000

    Modified SQL File
    Changes have been made and default values have been added. This is a
    critical piece of the program and has already been used to restore the
    DB. In case another failure happens, this should be the SQL file to
    re-create the tables from.

--- MySQL/Dump2014-01-07.sql
+++ MySQL/Dump2014-01-07.sql
@@ -1,10 +1,141 @@
+CREATE DATABASE  IF NOT EXISTS `srdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `srdb`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: 172.31.142.83    Database: srdb
+-- ------------------------------------------------------
+-- Server version	5.6.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auth`
+--
+
+DROP TABLE IF EXISTS `auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth` (
+  `username` varchar(45) DEFAULT NULL,
+  `pass` varchar(250) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  `surName` varchar(45) DEFAULT NULL,
+  `user_level` varchar(45) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth`
+--
+
+LOCK TABLES `auth` WRITE;
+/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
+INSERT INTO `auth` (`username`, `pass`, `email`, `firstName`, `surName`, `user_level`, `ID`) VALUES ('admin','c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc','dba@bobmail.com','admin','istratior','Admin',1);
+/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `records`
+--
+
+DROP TABLE IF EXISTS `records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `records` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  `surName` varchar(45) NOT NULL,
+  `address1` varchar(45) NOT NULL,
+  `address2` varchar(45) DEFAULT NULL,
+  `address3` varchar(45) DEFAULT NULL,
+  `postcode` varchar(45) NOT NULL,
+  `registration` varchar(45) NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `sold_by` varchar(45) NOT NULL,
+  `date_sold` varchar(100) NOT NULL,
+  `invoice_number` varchar(45) NOT NULL,
+  `sales_branch` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `payment_method` varchar(45) NOT NULL,
+  `total` decimal(20,2) NOT NULL,
+  `invoice_total` decimal(20,2) NOT NULL,
+  `number_of_services` varchar(45) NOT NULL,
+  `commission_amount` decimal(20,2) DEFAULT NULL,
+  `SRID` varchar(45) NOT NULL,
+  `comments` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  UNIQUE KEY `SRID_UNIQUE` (`SRID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `records`
+--
+
+LOCK TABLES `records` WRITE;
+/*!40000 ALTER TABLE `records` DISABLE KEYS */;
+INSERT INTO `records` (`ID`, `title`, `firstName`, `surName`, `address1`, `address2`, `address3`, `postcode`, `registration`, `model`, `sold_by`, `date_sold`, `invoice_number`, `sales_branch`, `type`, `payment_method`, `total`, `invoice_total`, `number_of_services`, `commission_amount`, `SRID`, `comments`) VALUES (1,'Mr','Admin','Istrator','Do not delete','Do not delete','Do not Delete','Do not Delete','Do not Delete','Do not Delete','Do not Delete','Do not Delete','Do not Delete','Do not Delete','Do not Delete','Do not Delete',100.00,100.00,'100',5.00,'100000','Do not Delete, this is what the SRID will be based off of');
+/*!40000 ALTER TABLE `records` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `services` (
+  `SRID` varchar(45) NOT NULL,
+  `date` varchar(45) DEFAULT NULL,
+  `title` varchar(45) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `surName` varchar(100) NOT NULL,
+  `registration` varchar(45) NOT NULL,
+  `amount` decimal(20,2) DEFAULT NULL,
+  `services_remaining` varchar(45) NOT NULL DEFAULT '0',
+  `services_left` varchar(45) NOT NULL DEFAULT 'TRUE',
+  `invoice_number` varchar(45) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-01-31 17:53:18
 CREATE DATABASE  IF NOT EXISTS `combobox` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `combobox`;
 -- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
--- Host: localhost    Database: combobox
+-- Host: 172.31.142.83    Database: combobox
 -- ------------------------------------------------------
--- Server version	5.6.14-log
+-- Server version	5.6.15-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -126,7 +257,7 @@ CREATE TABLE `type` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `type` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -135,7 +266,7 @@ CREATE TABLE `type` (
 
 LOCK TABLES `type` WRITE;
 /*!40000 ALTER TABLE `type` DISABLE KEYS */;
-INSERT INTO `type` (`ID`, `type`) VALUES (1,'New'),(2,'Used');
+INSERT INTO `type` (`ID`, `type`) VALUES (3,'New Basic'),(4,'New Premium'),(5,'Renewal Basic'),(6,'Renewal Premium');
 /*!40000 ALTER TABLE `type` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -148,138 +279,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-01-24 12:06:00
-CREATE DATABASE  IF NOT EXISTS `srdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `srdb`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
---
--- Host: localhost    Database: srdb
--- ------------------------------------------------------
--- Server version	5.6.14-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `auth`
---
-
-DROP TABLE IF EXISTS `auth`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `auth` (
-  `username` varchar(45) DEFAULT NULL,
-  `pass` varchar(250) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `firstName` varchar(45) DEFAULT NULL,
-  `surName` varchar(45) DEFAULT NULL,
-  `user_level` varchar(45) DEFAULT NULL,
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `auth`
---
-
-LOCK TABLES `auth` WRITE;
-/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
-INSERT INTO `auth` (`username`, `pass`, `email`, `firstName`, `surName`, `user_level`, `ID`) VALUES ('admin','c70b5dd9ebfb6f51d09d4132b7170c9d20750a7852f00680f65658f0310e810056e6763c34c9a00b0e940076f54495c169fc2302cceb312039271c43469507dc','dba@bobmail.com','admin','istratior','Admin',1);
-/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `records`
---
-
-DROP TABLE IF EXISTS `records`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `records` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(45) NOT NULL,
-  `surName` varchar(45) NOT NULL,
-  `address1` varchar(45) NOT NULL,
-  `address2` varchar(45) DEFAULT NULL,
-  `postcode` varchar(45) NOT NULL,
-  `registration` varchar(45) NOT NULL,
-  `model` varchar(45) NOT NULL,
-  `sold_by` varchar(45) NOT NULL,
-  `date_sold` varchar(100) NOT NULL,
-  `invoice_number` varchar(45) NOT NULL,
-  `sales_branch` varchar(45) NOT NULL,
-  `type` varchar(45) NOT NULL,
-  `payment_method` varchar(45) NOT NULL,
-  `total` decimal(20,2) NOT NULL,
-  `invoice_total` decimal(20,2) NOT NULL,
-  `number_of_services` int(11) NOT NULL,
-  `commission_amount` decimal(20,2) DEFAULT NULL,
-  `SRID` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`),
-  UNIQUE KEY `SRID_UNIQUE` (`SRID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `records`
---
-
-LOCK TABLES `records` WRITE;
-/*!40000 ALTER TABLE `records` DISABLE KEYS */;
-/*!40000 ALTER TABLE `records` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `services`
---
-
-DROP TABLE IF EXISTS `services`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `services` (
-  `SRID` varchar(45) NOT NULL,
-  `date` varchar(45) DEFAULT NULL,
-  `firstName` varchar(100) NOT NULL,
-  `surName` varchar(100) NOT NULL,
-  `registration` varchar(45) NOT NULL,
-  `amount` decimal(20,2) DEFAULT NULL,
-  `services_remaining` varchar(45) NOT NULL DEFAULT '0',
-  `services_left` varchar(45) NOT NULL DEFAULT 'TRUE',
-  `invoice_number` varchar(45) DEFAULT NULL,
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `services`
---
-
-LOCK TABLES `services` WRITE;
-/*!40000 ALTER TABLE `services` DISABLE KEYS */;
-/*!40000 ALTER TABLE `services` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-01-24 12:06:01
+-- Dump completed on 2014-01-31 17:53:19
