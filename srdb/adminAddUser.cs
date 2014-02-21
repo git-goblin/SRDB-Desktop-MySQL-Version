2e4006d877366212e90a43a7636536720653d41f
git-goblin <gitgoblin@gmail.com>
2014-02-21 13:05:00 +0000

    Added validation
    New validation added on the add user page so you can input a new user
    and check the username (although it does carry out validation for the
    rest of the form) before allowing the data to be sent to the DB

--- srdb/adminAddUser.cs
+++ srdb/adminAddUser.cs
@@ -27,19 +27,7 @@ namespace srdb
         private void btnAddUser_Click(object sender, EventArgs e)
         {
             try
-            {
-               int var1 = val.validate_name(txtFirstName.Text);
-               int var2 = val.validate_name(txtSurName.Text);
-               if (var1 != 1 && var2 !=1)
-                {
-                    return;
-                } 
-                if (txtPassword.Text == "" || txtPassword.TextLength < 7)
-                {
-                    MessageBox.Show("Password Cannot be blank and must be greater than 7 characters!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                    return;
-                } 
-                
+            { 
                 if (rbStandardUser.Checked)
                 {
                     user_level = "user";
@@ -47,18 +35,15 @@ namespace srdb
                 else if (rbAdministrator.Checked)
                 {
                     user_level = "Admin";
-                } 
-                string fn = txtFirstName.Text.ToLower();
-                string sn = txtSurName.Text.ToLower(); 
-
-                string username = fn + sn[0];
+                }
+                string username = txtUserName.Text;
                 
                 dbConnect.Initialize();
                 dbConnect.OpenConnection();
                 string query = "INSERT INTO auth (username, pass, email, firstName, surName, user_level) VALUES (@username, @password, @email, @firstName, @surname, @user_level)"; //(`username`, `pass`, `email`, `firstName`, `surName`, `user_level`)
                 using (MySqlCommand cmd = new MySqlCommand(query, dbConnect.connection))
                 {
-                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@username", username.ToLower());
                     cmd.Parameters.AddWithValue("@password", dbConnect.hash_value(txtPassword.Text));
                     cmd.Parameters.AddWithValue("@email", txtEmail.Text);
                     cmd.Parameters.AddWithValue("@firstName", txtFirstName.Text);
@@ -74,13 +59,50 @@ namespace srdb
                 MessageBox.Show("Error Adding User " + ex, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+        private int check_username()
+        {
+            try
+            {
+                dbConnect.Initialize();
+                dbConnect.OpenConnection();
+                int var1 = val.validate_name(txtFirstName.Text);
+                int var2 = val.validate_name(txtSurName.Text);
+                if (var1 != 1 && var2 != 1)
+                {
+                    return 0;
+                }
+                if (txtPassword.Text == "" || txtPassword.TextLength < 7)
+                {
+                    MessageBox.Show("Password Cannot be blank and must be greater than 7 characters!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    return 0;
+                } 
 
+                string check_query = "SELECT * FROM auth WHERE username=@username";
+                MySqlCommand check = new MySqlCommand(check_query, dbConnect.connection);
+                check.Parameters.AddWithValue("@username", txtUserName.Text);
+                if (check.ExecuteScalar() == null)
+                {
+                    return 1;
+                }
+                else
+                {
+                    MessageBox.Show("Username already exists, please choose another one", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex);
+                return 0;
+            }
+        }
         private void btnClearForm_Click(object sender, EventArgs e)
         {
             txtFirstName.Clear();
             txtSurName.Clear();
             txtEmail.Clear();
             txtPassword.Clear();
+            txtUserName.Clear();
         }
 
         private void btnMainMenu_Click(object sender, EventArgs e)
@@ -93,6 +115,22 @@ namespace srdb
         private void adminAddUser_Load(object sender, EventArgs e)
         {
             rbStandardUser.Checked = true;
+            btnAddUser.Enabled = false;
+        }
+
+        private void btnCheckUser_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.TextLength < 5)
+            {
+                MessageBox.Show("Invalid username! Must be at least 5 characters long", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int check = check_username();
+            if (check != 1)
+            {
+                return;
+            }
+            btnAddUser.Enabled = true;
         }
     }
 }
