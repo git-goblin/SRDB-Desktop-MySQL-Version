f6cf9ac58f8d0117cbbbeba23ea9e2cea3a7fbac
git-goblin <gitgoblin@gmail.com>
2014-02-24 17:02:27 +0000

    New form
    GUI for interacting with the config file

---
+++ srdb/adminServerConnections.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace srdb
+{
+    partial class adminServerConnections
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(adminServerConnections));
+            this.txtSave = new System.Windows.Forms.Button();
+            this.txtMainMenu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDBServer = new System.Windows.Forms.TextBox();
+            this.txtDBDB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDBUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDBPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLServer = new System.Windows.Forms.TextBox();
+            this.txtLDB = new System.Windows.Forms.TextBox();
+            this.txtLUser = new System.Windows.Forms.TextBox();
+            this.txtLPassword = new System.Windows.Forms.TextBox();
+            this.txtCBServer = new System.Windows.Forms.TextBox();
+            this.txtCBDB = new System.Windows.Forms.TextBox();
+            this.txtCBUser = new System.Windows.Forms.TextBox();
+            this.txtCBPassword = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSave
+            // 
+            this.txtSave.Location = new System.Drawing.Point(538, 79);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(75, 23);
+            this.txtSave.TabIndex = 0;
+            this.txtSave.Text = "Save";
+            this.txtSave.UseVisualStyleBackColor = true;
+            this.txtSave.Click += new System.EventHandler(this.txtSave_Click);
+            // 
+            // txtMainMenu
+            // 
+            this.txtMainMenu.Location = new System.Drawing.Point(538, 108);
+            this.txtMainMenu.Name = "txtMainMenu";
+            this.txtMainMenu.Size = new System.Drawing.Size(75, 23);
+            this.txtMainMenu.TabIndex = 1;
+            this.txtMainMenu.Text = "Main Menu";
+            this.txtMainMenu.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server:";
+            // 
+            // txtDBServer
+            // 
+            this.txtDBServer.Location = new System.Drawing.Point(69, 33);
+            this.txtDBServer.Name = "txtDBServer";
+            this.txtDBServer.Size = new System.Drawing.Size(100, 20);
+            this.txtDBServer.TabIndex = 3;
+            // 
+            // txtDBDB
+            // 
+            this.txtDBDB.Location = new System.Drawing.Point(69, 59);
+            this.txtDBDB.Name = "txtDBDB";
+            this.txtDBDB.Size = new System.Drawing.Size(100, 20);
+            this.txtDBDB.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Database:";
+            // 
+            // txtDBUser
+            // 
+            this.txtDBUser.Location = new System.Drawing.Point(69, 85);
+            this.txtDBUser.Name = "txtDBUser";
+            this.txtDBUser.Size = new System.Drawing.Size(100, 20);
+            this.txtDBUser.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Username:";
+            // 
+            // txtDBPass
+            // 
+            this.txtDBPass.Location = new System.Drawing.Point(69, 111);
+            this.txtDBPass.Name = "txtDBPass";
+            this.txtDBPass.Size = new System.Drawing.Size(100, 20);
+            this.txtDBPass.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password:";
+            // 
+            // txtLServer
+            // 
+            this.txtLServer.Location = new System.Drawing.Point(252, 33);
+            this.txtLServer.Name = "txtLServer";
+            this.txtLServer.Size = new System.Drawing.Size(100, 20);
+            this.txtLServer.TabIndex = 11;
+            // 
+            // txtLDB
+            // 
+            this.txtLDB.Location = new System.Drawing.Point(252, 59);
+            this.txtLDB.Name = "txtLDB";
+            this.txtLDB.Size = new System.Drawing.Size(100, 20);
+            this.txtLDB.TabIndex = 13;
+            // 
+            // txtLUser
+            // 
+            this.txtLUser.Location = new System.Drawing.Point(252, 85);
+            this.txtLUser.Name = "txtLUser";
+            this.txtLUser.Size = new System.Drawing.Size(100, 20);
+            this.txtLUser.TabIndex = 15;
+            // 
+            // txtLPassword
+            // 
+            this.txtLPassword.Location = new System.Drawing.Point(252, 111);
+            this.txtLPassword.Name = "txtLPassword";
+            this.txtLPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtLPassword.TabIndex = 17;
+            // 
+            // txtCBServer
+            // 
+            this.txtCBServer.Location = new System.Drawing.Point(432, 33);
+            this.txtCBServer.Name = "txtCBServer";
+            this.txtCBServer.Size = new System.Drawing.Size(100, 20);
+            this.txtCBServer.TabIndex = 19;
+            // 
+            // txtCBDB
+            // 
+            this.txtCBDB.Location = new System.Drawing.Point(432, 59);
+            this.txtCBDB.Name = "txtCBDB";
+            this.txtCBDB.Size = new System.Drawing.Size(100, 20);
+            this.txtCBDB.TabIndex = 21;
+            // 
+            // txtCBUser
+            // 
+            this.txtCBUser.Location = new System.Drawing.Point(432, 85);
+            this.txtCBUser.Name = "txtCBUser";
+            this.txtCBUser.Size = new System.Drawing.Size(100, 20);
+            this.txtCBUser.TabIndex = 23;
+            // 
+            // txtCBPassword
+            // 
+            this.txtCBPassword.Location = new System.Drawing.Point(432, 111);
+            this.txtCBPassword.Name = "txtCBPassword";
+            this.txtCBPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtCBPassword.TabIndex = 25;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(94, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Database";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(285, 9);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(33, 13);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "Login";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(452, 9);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(58, 13);
+            this.label15.TabIndex = 28;
+            this.label15.Text = "ComboBox";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Password:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(188, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Username:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(193, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Database:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(208, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Server:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(371, 114);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 36;
+            this.label9.Text = "Password:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(371, 88);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.TabIndex = 35;
+            this.label10.Text = "Username:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(370, 62);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 13);
+            this.label11.TabIndex = 34;
+            this.label11.Text = "Database:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(385, 36);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.TabIndex = 33;
+            this.label12.Text = "Server:";
+            // 
+            // adminServerConnections
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 143);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtCBPassword);
+            this.Controls.Add(this.txtCBUser);
+            this.Controls.Add(this.txtCBDB);
+            this.Controls.Add(this.txtCBServer);
+            this.Controls.Add(this.txtLPassword);
+            this.Controls.Add(this.txtLUser);
+            this.Controls.Add(this.txtLDB);
+            this.Controls.Add(this.txtLServer);
+            this.Controls.Add(this.txtDBPass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDBUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDBDB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDBServer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMainMenu);
+            this.Controls.Add(this.txtSave);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "adminServerConnections";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin Server Connections";
+            this.Load += new System.EventHandler(this.adminServerConnections_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button txtSave;
+        private System.Windows.Forms.Button txtMainMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDBServer;
+        private System.Windows.Forms.TextBox txtDBDB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDBUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDBPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLServer;
+        private System.Windows.Forms.TextBox txtLDB;
+        private System.Windows.Forms.TextBox txtLUser;
+        private System.Windows.Forms.TextBox txtLPassword;
+        private System.Windows.Forms.TextBox txtCBServer;
+        private System.Windows.Forms.TextBox txtCBDB;
+        private System.Windows.Forms.TextBox txtCBUser;
+        private System.Windows.Forms.TextBox txtCBPassword;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
---
+++ srdb/adminServerConnections.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace srdb
+{
+    public partial class adminServerConnections : Form
+    {
+        public adminServerConnections()
+        {
+            InitializeComponent();
+        }
+
+        private void adminServerConnections_Load(object sender, EventArgs e)
+        {
+            //on load, pull the values
+            
+            //ComboBox
+
+            txtCBDB.Text = ConfigurationManager.AppSettings["cb_database"];
+            txtCBPassword.Text = ConfigurationManager.AppSettings["cb_pass"];
+            txtCBServer.Text = ConfigurationManager.AppSettings["cb_server"];
+            txtCBUser.Text = ConfigurationManager.AppSettings["cb_username"];
+
+            //Database
+
+            txtDBDB.Text = ConfigurationManager.AppSettings["database"];
+            txtDBPass.Text = ConfigurationManager.AppSettings["pass"];
+            txtDBServer.Text = ConfigurationManager.AppSettings["server"];
+            txtDBUser.Text = ConfigurationManager.AppSettings["username"];
+
+            //Login
+
+            txtLDB.Text = ConfigurationManager.AppSettings["l_database"]; 
+            txtLPassword.Text = ConfigurationManager.AppSettings["l_pass"];
+            txtLServer.Text = ConfigurationManager.AppSettings["l_server"];
+            txtLUser.Text = ConfigurationManager.AppSettings["l_username"];
+        }
+
+        private void txtSave_Click(object sender, EventArgs e)
+        {
+            if (txtCBDB.Text != "" && txtCBPassword.Text != "" && txtCBServer.Text != "" && txtCBUser.Text != "" && txtDBDB.Text != "" && txtDBPass.Text != "" && txtDBServer.Text != "" && txtDBUser.Text != "" && txtLDB.Text != "" && txtLPassword.Text != "" && txtLServer.Text != "" && txtLUser.Text != "")
+            {
+                //do stuff here
+            }
+            else
+            {
+                MessageBox.Show("No fields can be left empty!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
